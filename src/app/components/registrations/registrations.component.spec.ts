7e01dbedd036dc3ae38873151dde2519177e2e46
santoshkumar2012 <web.santo2012@gmail.com>
2023-09-09 22:21:28 +0530

    Intilize registration component

---
+++ src/app/components/registrations/registrations.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrationsComponent } from './registrations.component';
+
+describe('RegistrationsComponent', () => {
+  let component: RegistrationsComponent;
+  let fixture: ComponentFixture<RegistrationsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegistrationsComponent]
+    });
+    fixture = TestBed.createComponent(RegistrationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
