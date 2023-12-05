cf663cbb4fccba6955272ee283a99b5718ac18b5
ak <theachyutkadam@gmail.com>
2023-12-05 22:14:20 +0530

    Added routes for show pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScorebookComponent } from './components/scorebook/scorebook.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationsComponent } from './components/registrations/registrations.component';
 
 const routes: Routes = [
   {path: "", redirectTo:"/home", pathMatch:"full"},
+  {path: "login", component:LoginComponent}
+  {path: "registration", component:RegistrationsComponent}
   {path: "scorebook", component:ScorebookComponent}
 ];
 
